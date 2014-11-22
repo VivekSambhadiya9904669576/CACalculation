16ed26c10cf602fe0c8986cc9dbaeb9ae8f79673
VivekSambhadiya9904669576 <viveksambhadiya@gmail.com>
2014-11-22 09:36:29 +0530

    Update CACal.c

--- CACal.c
+++ CACal.c
@@ -10,5 +10,22 @@ void main()
 	printf("\nEnter the Subject Name :");
 	scanf("%s",&name);
 
+	insert();
 	getch();
+}
+
+float insert()
+{
+	int ca1,ca2,ca3;
+	printf("Enter the Marks CA1 :");
+	scanf("%d",&ca1);
+	
+	
+	printf("Enter the Marks CA2 :");
+	scanf("%d",&ca2);
+	
+	
+	printf("Enter the Marks CA3 :");
+	scanf("%d",&ca3);
+
 }
